3a5385c2cc410ee34d8247dea581a884eb3e8976
Carlos Tiago <ontiago@gmail.com>
2020-02-14 18:41:16 -0300

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Carlos Tiago on 14/02/20.
+//  Copyright © 2020 Carlos Tiago. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
